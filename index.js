041010c74a893dd7f33773914968359e831c42ba
Francesco Novy <francesco@cropster.com>
2017-06-26 08:30:39 +0200

    Fix breaking changes in FastBoot 1.0
    Also update dependencies

--- index.js
+++ index.js
@@ -3,19 +3,41 @@
 
 'use strict';
 
+var fastbootTransform = require('fastboot-transform');
+var Funnel = require('broccoli-funnel');
+var Merge = require('broccoli-merge-trees');
+var path = require('path');
+var existsSync = require('exists-sync');
+
 module.exports = {
   name: 'ember-indexeddb',
 
   included: function(app) {
     this._super.included(app);
 
-    // In nested addons, app.bowerDirectory might not be available
-    var bowerDirectory = app.bowerDirectory || 'bower_components';
     // In ember-cli < 2.7, this.import is not available, so fall back to use app.import
     var importShim = typeof this.import !== 'undefined' ? this : app;
+    importShim.import('vendor/dexie/dexie.js');
+  },
 
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      importShim.import(bowerDirectory + '/dexie/dist/dexie.js');
+  treeForVendor(tree) {
+    let trees = [];
+
+    if (tree) {
+      trees.push(tree);
     }
+
+    var app = this._findHost();
+    var assetDir = path.join(this.project.root, app.bowerDirectory, 'dexie', 'dist');
+
+    if (existsSync(assetDir)) {
+      var browserTrees = fastbootTransform(new Funnel(assetDir, {
+        files: ['dexie.js'],
+        destDir: 'dexie'
+      }));
+      trees.push(browserTrees);
+    }
+
+    return new Merge(trees);
   }
 };
