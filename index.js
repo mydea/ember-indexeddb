295007f175e55a5dc13d4e60da8ccbbcbdda626a
Francesco Novy <francesco@cropster.com>
2017-12-20 11:44:46 +0100

    Update dependencies & update to dexie@2.x
    Also, load dexie from node_modules instead of bower.

--- index.js
+++ index.js
@@ -1,41 +1,57 @@
 /* eslint-env node */
 'use strict';
 
-var fastbootTransform = require('fastboot-transform');
-var Funnel = require('broccoli-funnel');
-var Merge = require('broccoli-merge-trees');
-var path = require('path');
-var existsSync = require('exists-sync');
+const fastbootTransform = require('fastboot-transform');
+const Funnel = require('broccoli-funnel');
+const MergeTrees = require('broccoli-merge-trees');
+const path = require('path');
 
 module.exports = {
   name: 'ember-indexeddb',
 
-  included: function(app) {
+  included(app) {
     this._super.included(app);
+    this._ensureThisImport();
 
-    // In ember-cli < 2.7, this.import is not available, so fall back to use app.import
-    var importShim = typeof this.import !== 'undefined' ? this : app;
-    importShim.import('vendor/dexie/dexie.js');
+    this.import('vendor/dexie/dexie.js');
   },
 
-  treeForVendor(tree) {
+  treeForVendor(vendorTree) {
+    let dexiePath = this._getPath();
+
     let trees = [];
-
-    if (tree) {
-      trees.push(tree);
+    if (vendorTree) {
+      trees.push(vendorTree);
     }
 
-    var app = this._findHost();
-    var assetDir = path.join(this.project.root, app.bowerDirectory, 'dexie', 'dist');
+    let dexieTree = fastbootTransform(new Funnel(dexiePath, {
+      include: ['dexie.js'],
+      destDir: 'dexie'
+    }));
 
-    if (existsSync(assetDir)) {
-      var browserTrees = fastbootTransform(new Funnel(assetDir, {
-        files: ['dexie.js'],
-        destDir: 'dexie'
-      }));
-      trees.push(browserTrees);
+    trees.push(dexieTree);
+
+    return new MergeTrees(trees, { overwrite: true });
+  },
+
+  _getPath() {
+    return path.dirname(require.resolve('dexie'));
+  },
+
+  _ensureThisImport() {
+    if (!this.import) {
+      this._findHost = function findHostShim() {
+        let current = this;
+        let app;
+        do {
+          app = current.app || app;
+        } while (current.parent.parent && (current = current.parent));
+        return app;
+      };
+      this.import = function importShim(asset, options) {
+        let app = this._findHost();
+        app.import(asset, options);
+      };
     }
-
-    return new Merge(trees);
   }
 };
